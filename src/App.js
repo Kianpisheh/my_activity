22d63471fbe59f422a219a0e72d507af08f2bd46
Kian Kianpisheh <mkian1368@gmail.com>
2022-06-01 00:07:37 -0400

    why-why_not-explanation added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import { useEffect, useState } from "react";
-import { explain, retrieveActivities, retrieveInstances } from "./APICalls/activityAPICalls";
+import {
+	explain,
+	retrieveActivities,
+	retrieveInstances,
+} from "./APICalls/activityAPICalls";
 
 import "./App.css";
 
-import AxiomManager from "./model/AxiomManager";
 import AxiomTypes from "./model/AxiomTypes";
 import Activity from "./model/Activity";
-import ActivityInstanceEvent from "./model/ActivityInstanceEvent";
 import ActivityInstance from "./model/ActivityInstance";
+import ExplanationManager from "./model/ExplanationManager";
 
 import ActivityAxiomPane from "./components/ActivityAxiomPane";
 import ActivityPane from "./components/ActivityPane";
 import ActivityInstanceVis from "./components/ActivityVis/ActivityInstanceVis";
 import ActivityInstancePane from "./components/ActivityInstancePane";
+import ActionMenu from "./components/ActionMenu";
+import { handleAxiomPaneMessages } from "./Handlers";
 
 function App() {
 	const [activities, setActivities] = useState([]);
@@ -20,50 +25,21 @@ function App() {
 	const [currentActivtyIdx, setCurrentActivityIdx] = useState(-1);
 	const [currentActInstanceIdx, setCurrentActInstanceIdx] = useState(-1);
 	const [scale, setScale] = useState(25);
+	const [action, setAction] = useState({
+		required: false,
+		x: 0,
+		y: 0,
+	});
 
-	function handleAxiomPaneMessages(message, values) {
-		if (message === AxiomTypes.MSG_CREATE_NEW_AXIOM) {
-			let new_axioms = AxiomManager.createAxiom(
-				currentActivity.getAxioms(),
-				values
-			);
-			let newActivities = [...activities];
-			newActivities[currentActivtyIdx].updateAxioms(new_axioms);
-			setActivities(newActivities);
-		} else if (message === AxiomTypes.MSG_AXIOM_CREATION_DONE) {
-			let new_axioms = AxiomManager.createAxiom(
-				currentActivity.getAxioms(),
-				values
-			);
-			let newActivities = [...activities];
-			newActivities[currentActivtyIdx].updateAxioms(new_axioms);
-			setActivities(newActivities);
-		} else if (message === AxiomTypes.MSG_TIME_CONSTRAINT_UPDATED) {
-			let new_axioms = AxiomManager.updateTimeConstraint(
-				values.id,
-				currentActivity.getAxioms(),
-				values.time,
-				values.type
-			);
-			let newActivities = [...activities];
-			newActivities[currentActivtyIdx].updateAxioms(new_axioms);
-			setActivities(newActivities);
-		} else if (message === AxiomTypes.MSG_TIME_CONSTRAINT_STATUS_UPDATED) {
-			let new_axioms = AxiomManager.updateTimeConstraintStatus(
-				values.id,
-				currentActivity.getAxioms(),
-				values.active,
-				values.type,
-				values.time
-			);
-			let newActivities = [...activities];
-			newActivities[currentActivtyIdx].updateAxioms(new_axioms);
-			setActivities(newActivities);
-		} else if (message === AxiomTypes.MSG_ACTIVITY_TITLE_UPDATED) {
-			let newActivities = [...activities];
-			newActivities[currentActivtyIdx]["name"] = values["title"];
-			setActivities(newActivities);
-		}
+	function onAxiomPaneMessage(message, values) {
+		let newActivities = handleAxiomPaneMessages(
+			message,
+			values,
+			activities,
+			currentActivtyIdx,
+			currentActivity
+		);
+		setActivities(newActivities);
 	}
 
 	let currentActivity = null;
@@ -100,10 +76,6 @@ function App() {
 	}
 
 	function handleActInstanceChange(id, instance) {
-		let promise = explain("http://localhost:8082/explainer/explain", instance, "BrushingTeeth", "why_not");
-		promise.then((data) => {
-			console.log("explanations: ", data.data)
-		})
 		setCurrentActInstanceIdx(id);
 	}
 
@@ -115,6 +87,26 @@ function App() {
 		} else if (action === "zoom_in") {
 			setScale(scale + 5);
 		}
+	}
+
+	function handleActionRequest(itemID, x, y) {
+		setAction({ required: true, x: x, y: y });
+		setCurrentActivityIdx(itemID);
+	}
+
+	function handleExplanationRequest(req) {
+		const url = "http://localhost:8082/explainer/explain";
+		let prom = explain(
+			url,
+			activityInstances[currentActInstanceIdx]["name"],
+			activities[currentActivtyIdx]["name"],
+			req.toLowerCase()
+		);
+
+		prom.then((data) => {
+			let explanations = ExplanationManager.getExplanations(data.data);
+
+		});
 	}
 
 	// load activities
@@ -133,9 +125,7 @@ function App() {
 	}, []);
 
 	useEffect(() => {
-		let instancesPromise = retrieveInstances(
-			"http://localhost:8082/instance"
-		);
+		let instancesPromise = retrieveInstances("http://localhost:8082/instance");
 		instancesPromise.then((data) => {
 			let instances = data.data;
 			let instanceItems = [];
@@ -145,59 +135,6 @@ function App() {
 			setActivityInstances(instanceItems);
 		});
 	}, []);
-
-	// test activity instance
-	const event1 = new ActivityInstanceEvent({
-		name: "stove",
-		start_time: 0.2,
-		end_time: 2,
-	});
-	const event2 = new ActivityInstanceEvent({
-		name: "mug",
-		start_time: 7,
-		end_time: 8,
-	});
-	const event3 = new ActivityInstanceEvent({
-		name: "pitcher",
-		start_time: 8.05,
-		end_time: 8.15,
-	});
-	const event4 = new ActivityInstanceEvent({
-		name: "water_bottle",
-		start_time: 10,
-		end_time: 10.15,
-	});
-	const event5 = new ActivityInstanceEvent({
-		name: "stove_lighter",
-		start_time: 10.15,
-		end_time: 10.3,
-	});
-	const event6 = new ActivityInstanceEvent({
-		name: "stove",
-		start_time: 15.15,
-		end_time: 15.9,
-	});
-	const event7 = new ActivityInstanceEvent({
-		name: "tooth_brush",
-		start_time: 19.15,
-		end_time: 22.9,
-	});
-	const event8 = new ActivityInstanceEvent({
-		name: "stovetop",
-		start_time: 27.15,
-		end_time: 28.9,
-	});
-	const activityInstance1 = new ActivityInstance({
-		name: "activity1",
-		events: [event1, event2, event3, event4, event5, event6, event7, event8],
-	});
-
-	const activityInstance2 = new ActivityInstance({
-		name: "activity1",
-		events: [event1, event2, event6, event7, event8],
-	});
-
-	//let activityInstances = [activityInstance1, activityInstance2];
 
 	const config = {
 		ic_w: 30,
@@ -214,11 +151,22 @@ function App() {
 		minor_tick_h: 2.5,
 	};
 
-	console.log(activityInstances);
-
 	return (
 		<div className="App">
-			<div className="left-pane-container">
+			<div
+				className="action-menue-container"
+				style={{ position: "absolute", left: action.x, top: action.y }}
+			>
+				{action.required && (
+					<ActionMenu
+						className="action-menue"
+						actions={["Why?", "Why not?"]}
+						onExplanationRequest={handleExplanationRequest}
+					></ActionMenu>
+				)}
+			</div>
+
+			<div className="left-pane-container" onClick={() => setAction({ required: false, x: 0, y: 0 })}>
 				<ActivityInstancePane
 					activtiyInstances={activityInstances}
 					onSelectedItemChange={handleActInstanceChange}
@@ -227,45 +175,28 @@ function App() {
 				<ActivityPane
 					activities={activities}
 					onActivitiyListChange={handleActivityListChange}
+					onAction={handleActionRequest}
 				></ActivityPane>
-
 			</div>
-			< div className="tools-container">
+
+			<div className="tools-container" onClick={() => setAction({ required: false, x: 0, y: 0 })}>
 				<ActivityInstanceVis
 					config={config}
 					activity={activityInstances[currentActInstanceIdx]}
 					onScaleChange={handleScaleChange}
 				></ActivityInstanceVis>
-				{currentActivity && (
-					<ActivityAxiomPane
-						width="400px"
-						activity={currentActivity}
-						sendMessage={handleAxiomPaneMessages}
-					></ActivityAxiomPane>
-				)}
+				<div className="axiom-pane-container">
+					{currentActivity && (
+						<ActivityAxiomPane
+							width="400px"
+							activity={currentActivity}
+							sendMessage={onAxiomPaneMessage}
+						></ActivityAxiomPane>
+					)}
+				</div>
 			</div>
 		</div>
 	);
 }
 
 export default App;
-
-// let database_axioms = [
-//   { id: 0, events: ["stovetop", "mug"], type: AxiomTypes.TYPE_INTERACTION },
-//   {
-//     id: 1,
-//     events: ["stovetop", "water_bottle"],
-//     type: AxiomTypes.TYPE_TIME_DISTANCE,
-//     th1: 5,
-//     th2: 11,
-//   },
-//   {
-//     id: 2,
-//     events: ["stove"],
-//     type: AxiomTypes.TYPE_DURATION,
-//     th1: 5,
-//     th2: 11,
-//   },
-// ];
-
-// list of activities
